b7d6bf7760bf082e0778398afa2e4715cc7c35e9
Malik Ata <malikata@fb.com>
2020-06-11 14:47:17 -0700

    Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import AppNavBar from "./components/AppNavBar";
 import Top from "./components/Top";
 import Recipe from "./components/Recipe";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <AppNavBar />
-        <Top />
-        <Recipe />
-      </div>
+      // Provider allows to share atate between components It proovides it
+      <Provider store={store}>
+        <div>
+          <AppNavBar />
+          <Top />
+          <Recipe />
+        </div>
+      </Provider>
     );
   }
 }
